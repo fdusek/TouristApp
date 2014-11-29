59f6f66476002b01d1d21b9332db878764e042d3
fdusek <filtr007@gmail.com>
2014-11-30 00:23:23 +0100

    I don't know

---
+++ TouristApp V3/Model/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristApp_V3.Model
+{
+    class Category
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public Category(String name)
+        {
+            _name = name;
+        }
+        public Category()
+        {
+        }
+
+    }
+}
---
+++ TouristApp V3/ViewModel/MainViewModel.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TouristApp_V3.Model;
+using Windows.UI.Xaml;
+
+namespace TouristApp_V3.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        #region Commands
+
+        public ICommand NewOrder
+        {
+            get { return new DelegateCommand(MakeOrder); }
+        }
+
+        public ICommand AddCategory
+        {
+            get { return new DelegateCommand(AddCat); }
+        }
+
+        public ICommand AddItem
+        {
+            get { return new DelegateCommand(AddIt); }
+        }
+
+        public ICommand IncOrder
+        {
+            get { return new DelegateCommand(IncreaseOrder); }
+        }
+
+        public ICommand DecOrder
+        {
+            get { return new DelegateCommand(DecreaseOrder); }
+        }
+
+        public void IncreaseOrder()
+        {
+            if (_selectedItem != null)
+            {
+                _selectedItem.OrderQuantity += 1;
+                RaisePropertyChangedEvent("SelectedItem");
+            }
+        }
+
+        public void DecreaseOrder()
+        {
+            if (_selectedItem != null)
+            {
+                _selectedItem.OrderQuantity -= 1;
+                RaisePropertyChangedEvent("SelectedItem");
+            }
+        }
+
+        public async void AddCat()
+        {
+            await new Windows.UI.Popups.MessageDialog("hovno").ShowAsync();
+        }
+
+        public async void AddIt()
+        {
+            await new Windows.UI.Popups.MessageDialog("Coming soon ;)").ShowAsync();
+        }
+
+        public void MakeOrder()
+        {
+            foreach (Item item in _items)
+            {
+                if (item.OrderQuantity > 0)
+                {
+                    OrderViewModel.OrderItems.Add(new OrderItem(item.Name, item.Price, item.OrderQuantity));
+                }
+            }
+
+            int total = 0;
+            foreach (var Item in OrderViewModel.OrderItems)
+            {
+                total += Item.Price * Item.Quantity;
+            }
+            OrderViewModel.Total.String = Convert.ToString(total);
+        }
+        #endregion
+
+        #region Collections
+        //PRIVATE DECLARATIONS OF COLLECTIONS
+
+        //Collection with food categories (first column on mainpage)
+        private ObservableCollection<Category> _foodCategories = new ObservableCollection<Category>();
+        //Collection with meals (first column on MainPage.xaml)
+        //NOTE meals class should be changed to items, items class will be universal and it will be common for meals and drinks
+        private ObservableCollection<Item> _items = new ObservableCollection<Item>();
+
+        //PUBLIC DECLARATIONS OF COLLECTIONS (can be accessed from xaml etc.)
+
+        public ObservableCollection<Category> FoodCategories
+        {
+            get
+            {
+                return _foodCategories;
+            }
+        }
+        public ObservableCollection<Item> Items
+        {
+            get //when xaml wants to get list of meals
+            {
+                ObservableCollection<Item> temp = new ObservableCollection<Item>(); //create new temporary list
+                if (_selectedCategory != null) // check if something is selected, if its then skip
+                {
+                    foreach (Item item in _items) //loop through all meals, no matter what category are they
+                    {
+                        if (item.CategoryName == _selectedCategory.Name) //if the category matches with selected category
+                        {
+                            temp.Add(item); //add it to temporary list
+                        }
+                    }
+                }
+                return temp; //return temporary list, this will be displayed in app (mainpage.xaml second column)
+            }
+        }
+        #endregion
+
+        #region Selected items/categories
+        private Category _selectedCategory;
+        private Item _selectedItem;
+
+        //new category was selected
+        public Category SelectedCategory
+        {
+            get
+            {
+                return _selectedCategory;
+            }
+            set
+            {
+                _selectedCategory = value;
+                RaisePropertyChangedEvent("Items");
+                //list of meals should be changed to show only meals in selected category
+                //so we call RaisePropertyChanged with argument "Items" which refreshes everything binded to Items
+            }
+        }
+
+        //some meal was selected
+        public Item SelectedItem
+        {
+            get
+            {
+                return _selectedItem;
+            }
+            set
+            {
+                _selectedItem = value;
+                if (_selectedItem != null) //if selected item isnt null (if there is some item selected)
+                {
+                    _showDetail = true; //set showdetail to true, this means, that program will show 3 column on mainpage.xaml
+                }
+                else
+                {
+                    _showDetail = false;
+                }
+                RaisePropertyChangedEvent("ShowDetail");
+                RaisePropertyChangedEvent("SelectedItem");
+            }
+        }
+        #endregion
+
+        #region Rest
+        public MainViewModel()
+        {
+
+            _items = new ObservableCollection<Item>();
+            using (var db = new SQLite.SQLiteConnection(App.DBPath))
+            {
+                var query = db.Table<Item>().OrderBy(c => c.Name);
+                foreach (var _item in query)
+                {
+                    var tempItem = new Item()
+                    {
+                        ID = _item.ID,
+                        CategoryName = _item.CategoryName,
+                        DES = _item.DES,
+                        Image = _item.Image,
+                        Name = _item.Name,
+                        OrderQuantity = 0,
+                        Price = _item.Price
+                    };
+                    _items.Add(tempItem);
+                }
+
+                var query2 = db.Table<Category>().OrderBy(c => c.Name);
+                foreach (var _item in query2)
+                {
+                    _foodCategories.Add(new Category(_item.Name));
+                }
+            }
+
+            _showDetail = false;
+        }
+        private bool _showDetail;
+        public object ShowDetail
+        {
+            get
+            {
+                if (_showDetail) //visibility isnt boolean but Visibility.Visible and Visibility.Collapsed so we translate boolean to these things by this if else statement
+                {
+                    return Visibility.Visible;
+                }
+                return Visibility.Collapsed;
+            }
+            set { }
+        }
+        #endregion
+    }
+}
